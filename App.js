5743467832a6a3d5b48ac9dac312d363400c5b50
timbaron <timothy.akiode@student.aul.edu.ng>
2022-04-30 22:17:30 +0100

    Change color with buttons

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [bgcolor, setBgolor] = useState('lightblue');
+  const changeColor = (color) => {
+    setBgolor(color)
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={[styles.container, { backgroundColor: bgcolor }]}>
+      <Text>Click the buttons to change color</Text>
+      <View style={styles.buttons}>
+        <Button title="Grey" onPress={() => changeColor('grey')} />
+        {/* <Button title="purple" onPress={() => changeColor('purple')} />
+      <Button title="pink" onPress={() => changeColor('pink')}/> */}
+      </View>
+      <View style={styles.buttons}>
+        <Button title="purple" onPress={() => changeColor('purple')} />
+      </View>
+      <View style={styles.buttons}>
+        <Button title="Pink" onPress={() => changeColor('pink')} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttons:{
+    marginBottom:10,
+    alignItems:'center',
+  }
 });
